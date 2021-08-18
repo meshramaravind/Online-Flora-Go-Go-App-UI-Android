6e7ae99c4e69adafa36eae806a76955bf6b5fd41
Arvind Meshram <arvind.isync@gmail.com>
2021-08-18 16:48:33 +0530

    Initial commit

--- app/src/main/java/com/arvind/jetcomposeloginui/view/HomeScreen.kt
+++ app/src/main/java/com/arvind/jetcomposeloginui/view/HomeScreen.kt
@@ -19,7 +19,6 @@ import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -261,11 +260,11 @@ fun PopularItemSection(navController: NavController) {
         }
     }
     Spacer(modifier = Modifier.padding(10.dp))
-    PouplarItems()
+    PouplarItems(navController)
 }
 
 @Composable
-fun PouplarItems() {
+fun PouplarItems(navController: NavController) {
     Row(
         modifier = Modifier
             .fillMaxWidth()
@@ -278,6 +277,7 @@ fun PouplarItems() {
                 .wrapContentHeight()
                 .clip(RoundedCornerShape(16.dp))
                 .background(white)
+                .clickable { navController.navigate(Screen.DetailsScreen.route) }
         ) {
             Column(
                 modifier = Modifier
@@ -351,6 +351,7 @@ fun PouplarItems() {
                 .wrapContentHeight()
                 .clip(RoundedCornerShape(16.dp))
                 .background(white)
+                .clickable { navController.navigate(Screen.DetailsScreen.route) }
         ) {
             Column(
                 modifier = Modifier
